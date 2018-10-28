9e4ab8f5bfcf908253e30910324613f51e567bd1
Dan-Baba <dan.foote104227@gmail.com>
2018-10-27 22:47:49 -0400

    Single Post Page Now Has Comments!
    Now displaying comments properly.

---
+++ ThisLookInfected/ThisLookInfected/Comment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThisLookInfected
+{
+    public class Comment
+    {
+        public string Name { get; set; }
+        public string Date { get; set; }
+        public string CommentText { get; set; }
+        public List<Comment> Responses { get; set; }
+    }
+}
